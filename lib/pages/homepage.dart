2befb0181cc3abf88f54109fc61d9853df8d47fc
AnaghaS-Shastri <149950166+AnaghaS-Shastri@users.noreply.github.com>
2024-01-14 09:22:32 +0530

    Homepage Bars Ready

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -10,11 +10,6 @@ class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Home Page'),
-        backgroundColor: Colors.indigoAccent,
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -26,25 +21,58 @@ class _homepageState extends State<homepage> {
             child: Stack(
           children: [
                Image(
-                 image: NetworkImage('https://t3.ftcdn.net/jpg/00/35/84/88/160_F_35848809_NDtD2U8xbxDL1TW4e2qWR1HX0ZIi12aF.jpg'),
-                  width: 300,
+                 image: NetworkImage('https://t3.ftcdn.net/jpg/00/35/84/88/160_F_35848809_NDtD2U8xbxDL1TW4e2qWR1HX0ZIi12aF.jpg'), //profile bar
+                  width: 200,
                   height: 100,
              ),
             ],
           ),
           ),
-          ElevatedButton(onPressed: () {},
-              child: Text('JOIN A COMMUNITY'),
+          InkWell(
+            onTap: () {
+    // do something when the button is pressed
+    },
+             child: Stack(
+             children: [
+                 Image(
+                    image: NetworkImage('https://www.carnegiefoundation.org/wp-content/uploads/2022/03/New-Bizz-Featured-Image.png'), //join community bar
+                    width: 200,
+                    height: 100,
+       ),
+          ],
           ),
-          ElevatedButton(onPressed: () {},
-              child: Text('RECYCLE PLASTIC'),
           ),
-          ElevatedButton(onPressed: () {},
-          child: Text('EDUCATIONAL RESOURCES'),
-          ),
+          InkWell(
+              onTap: () {
+              // do something when the button is pressed
+              },
+              child: Stack(
+              children: [
+                  Image(
+                    image: NetworkImage('https://industryvisuals.com/wp-content/uploads/2018/10/Recycle-plastic-768x761.png'),//recycle plastic bar
+                    width: 200,
+                    height: 100,
+              ),
+               ],
+              ),
+                      ),
+            InkWell(
+            onTap: () {
+            // do something when the button is pressed
+            },
+                child: Stack(
+                children: [
+                    Image(
+                      image: NetworkImage('https://static.vecteezy.com/system/resources/previews/000/412/173/non_2x/people-with-education-related-icons-vector.jpg'), //education
+                      width: 200,
+                      height: 100,
+              ),
+              ],
+                ),
+                  ),
 
 
-        ],
+                ],
       ),
     );
   }
