3314bf0252691a25fa06e83067de1d81917b7ec3
Vinayak N Magajikondi <88372964+VINAYAK-N-MAGAJIKONDI@users.noreply.github.com>
2024-01-15 00:06:28 +0530

    Update homepage.dart

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
-import 'maps.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'map.dart';
+
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
 
@@ -14,119 +12,85 @@ class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          // Set the background color to aqua blue
-          title: Text(
-            'Discover Projects',
-            style: TextStyle(
-              fontWeight: FontWeight.bold, // Set text to bold
-              color: Colors.white, // Set text color to white
-            ),
+      appBar: AppBar(
+        backgroundColor:
+        Colors.blueAccent, // Set the background color to aqua blue
+        title: Text(
+          'Discover Projects',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, // Set text to bold
+            color: Colors.white, // Set text color to white
           ),
         ),
-
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            InkWell(
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          InkWell(
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => CleanupLocationsScreen()),
+                MaterialPageRoute(
+                    builder: (context) => CleanupLocationsScreen()),
               );
               // Do something when the button is tapped
             },
             child: Ink.image(
-              image: NetworkImage('https://i.imgur.com/Pf1B3Q1.jpeg'), // Use AssetImage to load the image
+              image: NetworkImage(
+                  'https://i.imgur.com/Pf1B3Q1.jpeg'), // Use AssetImage to load the image
               fit: BoxFit.contain,
               width: 300,
               height: 150,
-
             ),
-
           ),
           Center(
             child: InkWell(
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MapsPage()),
-                );
                 // Do something when the button is tapped
               },
+              child: Ink.image(
+                image: NetworkImage(
+                    'https://www.carnegiefoundation.org/wp-content/uploads/2022/03/New-Bizz-Featured-Image.png'),
+                fit: BoxFit.cover,
+                width: 200,
+                height: 100,
+              ),
             ),
-    ),
-            ListView(
-              children: [
-                SizedBox(height: 80,),
-                CarouselSlider(items: [
-                  Container(
-                    margin: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage('https://live.staticflickr.com/3484/3824082777_e5b41152dc_n.jpg'),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(25),
-
-                    ),
-                  ),
-
-                  Container(
-                    margin: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage('https://media.istockphoto.com/id/908255262/photo/traditional-moroccan-market-with-souvenirs.webp?b=1&s=170667a&w=0&k=20&c=fjiXOH2EuAR4BhVZXJBpsQgtd1ssQ15MgrMfsIgw45o='),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(25),
-
-                    ),
-                  ),
-
-                  Container(
-                    margin: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage('https://media.istockphoto.com/id/517377154/photo/elephant-toys-in-the-shop.webp?b=1&s=170667a&w=0&k=20&c=kJoY1Yh9mOmk0xma7SFBM7FetZ8hoeV8AHJ2ZWHV_OI='),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(25),
-
-                    ),
-                  ),
-
-                  Container(
-                    margin: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage('https://images.unsplash.com/photo-1606689518099-4437c5dd3b21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTh8MTE0Nzg5MHx8ZW58MHx8fHx8'),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(25),
-
-                    ),
-                  ),
-
-                ], options: CarouselOptions(
-                  height: 180,
-                  aspectRatio: 16/8,
-                  viewportFraction: 8.6,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 5),
-                  autoPlayAnimationDuration: Duration(microseconds: 800),
-                  reverse: true,
-
-
-                 ))
-
-              ],
+          ),
+          Center(
+            child: InkWell(
+              onTap: () {
+                // Do something when the button is tapped
+              },
+              child: Ink.image(
+                image: NetworkImage(
+                    'https://www.carnegiefoundation.org/wp-content/uploads/2022/03/New-Bizz-Featured-Image.png'),
+                fit: BoxFit.cover,
+                width: 200,
+                height: 100,
+              ),
             ),
-          ],
-        ),
-
+          ),
+          Center(
+            child: InkWell(
+              onTap: () {
+                // do something when the button is pressed
+              },
+              child: Stack(
+                children: [
+                  Image(
+                    image: NetworkImage(
+                        'https://static.vecteezy.com/system/resources/previews/000/412/173/non_2x/people-with-education-related-icons-vector.jpg'), //education
+                    width: 200,
+                    height: 100,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
