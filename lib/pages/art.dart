66c7e06df78c89ac3dfc5c574ce7f144ea8c558b
AnaghaS-Shastri <149950166+AnaghaS-Shastri@users.noreply.github.com>
2024-01-15 13:19:58 +0530

    trial art

--- lib/pages/art.dart
+++ lib/pages/art.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class art extends StatefulWidget {
   const art({Key? key}) : super(key: key);
@@ -13,6 +14,58 @@ class _artState extends State<art> {
       appBar: AppBar(
         title: Text("Arts and crafts"),
       ),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(100)
+                )
+              ),
+              child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+              children: [
+                itemDashboard('Videos', CupertinoIcons.play_rectangle, Colors.deepOrange)
+
+              ],),
+            ),
+          )
+        ],
+      )
     );
   }
+  itemDashboard(String title, IconData iconData, Color background) => Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(00, 5),
+            color: Theme.of(context).primaryColor.withOpacity(.2),
+            spreadRadius: 2,
+            blurRadius: 5
+          )
+        ]
+  ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: background,
+            shape: BoxShape.circle
+          ),
+          child: Icon(iconData, color: Colors.white),
+        ),
+        const SizedBox( height: 8),
+        Text(title, style: Theme.of(context).textTheme.titleMedium,)
+      ],
+    ),
+  );
 }
