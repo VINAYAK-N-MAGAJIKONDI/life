2fc613b8c9ef60912c2a930bea5b480125450dd2
voltagevalkyrie <149293731+voltagevalkyrie@users.noreply.github.com>
2024-01-15 23:09:36 +0530

    initial commit
    this is edu.dart page

--- lib/pages/edu.dart
+++ lib/pages/edu.dart
@@ -1,19 +1,74 @@
 import 'package:flutter/material.dart';
-class edu extends StatefulWidget {
-  const edu({super.key});
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-  @override
-  State<edu> createState() => _eduState();
+void main() {
+  runApp(MyApp());
 }
 
-class _eduState extends State<edu> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('EDUCATIONAL RESOURCES'),
-
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EDUCATIONAL RESOURCES'),
+        ),
+        body: Center(
+          child: MyYouTubePlayer(),
+        ),
       ),
     );
+  }
+}
+
+class MyYouTubePlayer extends StatefulWidget {
+  @override
+  _MyYouTubePlayerState createState() => _MyYouTubePlayerState();
+}
+
+class _MyYouTubePlayerState extends State<MyYouTubePlayer> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = YoutubePlayerController(
+      initialVideoId: 'iLnmTe5Q2Qw',
+      flags: YoutubePlayerFlags(
+        autoPlay: true,
+        mute: true,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+       // videoProgressIndicatorColor: Colors.amber,
+
+        onReady: () {
+          // You can add any onReady logic here
+        },
+      ),
+      builder: (context, player) {
+        return Column(
+          children: [
+            // Some other widgets can be added above the player if needed
+            player,
+            // Some other widgets can be added below the player if needed
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
