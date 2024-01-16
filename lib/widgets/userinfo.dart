34af322df80c41f42aa7a693b47a516ef11c1061
HemaShenoy <122464897+HemaShenoy@users.noreply.github.com>
2024-01-16 07:21:05 +0530

    profile page
    my stats

--- lib/widgets/userinfo.dart
+++ lib/widgets/userinfo.dart
@@ -55,7 +55,7 @@ class UserInfoWidget extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Userstat(),
+            child: ProfilePage(),
           ),
         ],
       ),
