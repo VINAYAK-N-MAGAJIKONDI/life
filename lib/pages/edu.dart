ccfd7b1da3f402fd0b2935952eb3e8d6b19a6798
Vinayak N Magajikondi <88372964+VINAYAK-N-MAGAJIKONDI@users.noreply.github.com>
2024-01-17 00:10:15 +0530

    fixed fatal error

--- lib/pages/edu.dart
+++ lib/pages/edu.dart
@@ -13,56 +13,7 @@ class edu extends StatelessWidget {
         title: Text('EDUCATIONAL RESOURCES'),
       ),
 
-
-      body: MyHomePage(),
     );}}
-
-class MyHomePage extends StatelessWidget {
-  final List<Map<String, String>> videoList = [
-    {
-      'title': 'Video 1',
-      'url': 'https://www.youtube.com/watch?v=VIDEO_ID_1',
-    },
-    {
-      'title': 'Video 2',
-      'url': 'https://www.youtube.com/watch?v=VIDEO_ID_2',
-    },
-    {
-      'title': 'Video 3',
-      'url': 'https://www.youtube.com/watch?v=VIDEO_ID_3',
-    },
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('YouTube Videos'),
-      ),
-      body: ListView.builder(
-        itemCount: videoList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(videoList[index]['title']!),
-            onTap: () => _launchURL(videoList[index]['url']!),
-          );
-        },
-      ),
-    );
-  }
-
-  _launchURL(String url) async {
-    String url = videoList.url;
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Could not launch the url '),
-          ),);
-    }
-  }
-}
 
 
 
--- lib/pages/userstat.dart
+++ lib/pages/userstat.dart
@@ -1,16 +1,83 @@
 import 'package:flutter/material.dart';
-class Userstat extends StatefulWidget {
-  const Userstat({Key? key}) : super(key: key);
+import 'package:pie_chart/pie_chart.dart';
 
-  @override
-  State<Userstat> createState() => _UserstatState();
-}
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
 
-class _UserstatState extends State<Userstat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('hemaaa '),
+      appBar: AppBar(
+        title: const Text('My Profile'),
+        // Remove the IconButton below to remove the logout button
+        // actions: [
+        //   IconButton(
+        //     icon: Icon(Icons.logout),
+        //     onPressed: () {
+        //       // Logout logic here
+        //     },
+        //   ),
+        // ],
+      ),
+      body: Column(
+        children: [
+          const ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text('My Stats'),
+            subtitle: Text('Wallet cash: 100rs\nNumber of coupons: 2'),
+          ),
+          Expanded(
+            child: Center(
+              child: PieChart(
+                dataMap: const {
+                  'Cleanups': 40,
+                  'Recycle': 30,
+                  'Donation': 30,
+                },
+                chartLegendSpacing: 32,
+                chartRadius: MediaQuery.of(context).size.width / 3.2,
+                colorList: const [
+                  Colors.yellow,
+                  Colors.green,
+                  Colors.blue,
+                ],
+                initialAngleInDegree: 0,
+                chartType: ChartType.disc,
+              ),
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CouponCard(
+                discount: '10% off',
+                // Other properties here
+              ),
+              CouponCard(
+                discount: '30% off',
+                // Other properties here
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CouponCard extends StatelessWidget {
+  final String discount;
+  // Other properties here
+
+  const CouponCard({Key? key, required this.discount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(discount),
+      ),
     );
   }
 }
