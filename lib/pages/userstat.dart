5b878f2cf3cf681e0bc1747fb40bda0b4020d254
HemaShenoy <122464897+HemaShenoy@users.noreply.github.com>
2024-01-17 09:17:52 +0530

    profile page changes

--- lib/pages/userstat.dart
+++ lib/pages/userstat.dart
@@ -1,33 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfilePage(),
+    );
+  }
+}
+
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Profile'),
-        // Remove the IconButton below to remove the logout button
-        // actions: [
-        //   IconButton(
-        //     icon: Icon(Icons.logout),
-        //     onPressed: () {
-        //       // Logout logic here
-        //     },
-        //   ),
-        // ],
-      ),
-      body: Column(
-        children: [
-          const ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('My Stats'),
-            subtitle: Text('Wallet cash: 100rs\nNumber of coupons: 2'),
-          ),
-          Expanded(
-            child: Center(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CouponCard(
+                  discount: 'Wallet Cash',
+                ),
+                CouponCard(
+                  discount: 'Number of coupons',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
               child: PieChart(
                 dataMap: const {
                   'Cleanups': 40,
@@ -45,21 +58,12 @@ class ProfilePage extends StatelessWidget {
                 chartType: ChartType.disc,
               ),
             ),
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CouponCard(
-                discount: '10% off',
-                // Other properties here
-              ),
-              CouponCard(
-                discount: '30% off',
-                // Other properties here
-              ),
-            ],
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            const RewardSection(),
+          ],
+        ),
       ),
     );
   }
@@ -67,7 +71,6 @@ class ProfilePage extends StatelessWidget {
 
 class CouponCard extends StatelessWidget {
   final String discount;
-  // Other properties here
 
   const CouponCard({Key? key, required this.discount}) : super(key: key);
 
@@ -78,6 +81,74 @@ class CouponCard extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Text(discount),
       ),
+    );
+  }
+}
+
+class Reward {
+  final String brand;
+  final int discount;
+
+  Reward(this.brand, this.discount);
+}
+
+class RewardItem extends StatelessWidget {
+  final Reward reward;
+
+  const RewardItem(this.reward);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.brown.shade50, // Background color
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          SizedBox(width: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                reward.brand,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text(
+                '${reward.discount}% off',
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RewardSection extends StatelessWidget {
+  const RewardSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Reward> rewards = [
+      Reward('Trends', 10),
+      Reward('Zomato', 20),
+      Reward('Lenskart', 15),
+      Reward('Amazon', 25),
+      Reward('Starbucks', 15),
+      Reward('Nike', 30),
+    ];
+
+    return Column(
+      children: [
+        const Text('Rewards', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+        Column(
+          children: rewards.map((reward) => RewardItem(reward)).toList(),
+        ),
+      ],
     );
   }
 }
