6a9716fa655b865ac0c4a4df9d7b5dd4f3e9e654
Vinayak N Magajikondi <88372964+VINAYAK-N-MAGAJIKONDI@users.noreply.github.com>
2024-02-11 22:10:51 +0530

    Update userinfo.dart.   App bar added

--- lib/widgets/userinfo.dart
+++ lib/widgets/userinfo.dart
@@ -9,56 +9,63 @@ class UserInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(user?.photoURL ?? ""),
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            user?.displayName ?? "User Name",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Profile",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(user?.photoURL ?? ""),
             ),
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            user?.email ?? "UserEmail@gmail.com",
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
+            SizedBox(height: 16.0),
+            Text(
+              user?.displayName ?? "User Name",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SizedBox(height: 16.0),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
+            SizedBox(height: 8.0),
+            Text(
+              user?.email ?? "UserEmail@gmail.com",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
             ),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Sign Out",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
+              ),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Sign Out",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: ProfilePage(),
-          ),
-
-        ],
+            Expanded(
+              child: ProfilePage(),
+            ),
+          ],
+        ),
       ),
     );
   }
